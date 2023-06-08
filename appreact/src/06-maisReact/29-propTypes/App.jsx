0e515e895eb3444adf211c9c4f87023be7b0d945
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-08 07:46:12 -0300

    Mais React - PropTypes

---
+++ appreact/src/06-maisReact/29-propTypes/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './06-maisReact/29-propTypes/Button';
+
+const App = () => {
+  return (
+    <>
+      <Button width={300}>Clique Aqui</Button>
+    </>
+  );
+};
+
+export default App;
